b41fab0c21d57cd74875d74cb3f172942545eab5
Kirill Müller <kirill@cynkra.com>
2024-10-20 08:39:57 +0200

    Code

--- src/code.cpp
+++ src/code.cpp
@@ -1,5 +1,27 @@
 #include <cpp11.hpp>
+
+#include <unistd.h>
+
 using namespace cpp11;
 
+extern "C" {
+
+// flag indicating whether R interrupts are pending
+#ifndef _WIN32
+extern int R_interrupts_pending;
+#else
+LibExtern int UserBreak;
+#endif
+
+};
+
+
 [[cpp11::register]]
-void fun() {}
+void fun() {
+  for (int i = 0; i < 10000; i++) {
+    usleep(1000);
+    if (R_interrupts_pending) {
+      cpp11::stop("Interrupted!");
+    }
+  }
+}
